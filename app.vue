ab6f5ea4eb8b0c1845143ff88612ac57199ba103
chle <mostwantedhashir@gmail.com>
2025-11-17 14:26:59 +0500

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,28 +2,28 @@
   <Head>
     <Title>chle!</Title>
   </Head>
+
   <NuxtLayout>
     <Backdrop />
-<svg
-  width="32"
-  viewBox="0 0 200 80"
-  class="fill-overlay2 object-contain"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <text
-    x="0"
-    y="60"
-    font-family="sans-serif"
-    font-size="60"
-    class="fill-inherit"
-  >
-    chle
-  </text>
-</svg>
-    
-        <NuxtPage />
-      </div>
-    </div>
+
+    <svg
+      width="32"
+      viewBox="0 0 200 80"
+      class="fill-overlay2 object-contain"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <text
+        x="0"
+        y="60"
+        font-family="sans-serif"
+        font-size="60"
+        class="fill-inherit"
+      >
+        chle
+      </text>
+    </svg>
+
+    <NuxtPage />
   </NuxtLayout>
 </template>
 

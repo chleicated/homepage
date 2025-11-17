c79656b148384cb6c39d5fc48f68ba1670b75076
chle <mostwantedhashir@gmail.com>
2025-11-17 14:29:12 +0500

    lol

--- app.vue
+++ app.vue
@@ -6,24 +6,34 @@
   <NuxtLayout>
     <Backdrop />
 
-    <svg
-      width="32"
-      viewBox="0 0 200 80"
-      class="fill-overlay2 object-contain"
-      xmlns="http://www.w3.org/2000/svg"
+    <div
+      class="fixed left-0 top-0 grid h-screen w-screen place-items-center overflow-y-scroll bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-transparent to-crust"
     >
-      <text
-        x="0"
-        y="60"
-        font-family="sans-serif"
-        font-size="60"
-        class="fill-inherit"
+      <div
+        class="container max-w-screen-sm border border-overlay2 bg-crust p-12"
       >
-        chle
-      </text>
-    </svg>
 
-    <NuxtPage />
+        <svg
+          width="32"
+          viewBox="0 0 200 80"
+          class="fill-overlay2 object-contain"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <text
+            x="0"
+            y="60"
+            font-family="sans-serif"
+            font-size="60"
+            class="fill-inherit"
+          >
+            chle
+          </text>
+        </svg>
+
+        <NuxtPage />
+
+      </div>
+    </div>
   </NuxtLayout>
 </template>
 
@@ -32,9 +42,6 @@ import Backdrop from "~/components/Backdrop.vue";
 import AnimatedLink from "~/components/AnimatedLink.vue";
 
 export default {
-  components: {
-    Backdrop,
-    AnimatedLink,
-  },
+  components: { Backdrop, AnimatedLink },
 };
 </script>

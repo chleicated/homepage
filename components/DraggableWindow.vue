983677be79f3635209a01668df15c25518cff5c0
Guevara <mostwantedhashir@gmail.com>
2025-12-04 15:23:11 +0500

    Update DraggableWindow.vue

--- components/DraggableWindow.vue
+++ components/DraggableWindow.vue
@@ -12,25 +12,29 @@
           <div class="wrapper" id="wrapper2">
             <div class="inner">
               <h2 id="windowGreetings">Greetings!</h2>
-              <p id="windowText">
-  <span class="p">
-    Hi, I'm Guevara (also known as Chle) - a insane developer from Spain.
-    Over few years I have been developing some random shit on internet.
-  </span>
-  <span class="p">
-    I am fluent in PHP and Javascript/Typescript. I have got some experience
-    with React, Vue and SQL. I can do bit of Go and Lua too, sometimes rust
-    although they're not on my list.
-  </span>
-</p>
 
-<p id="windowText">
-  <span class="p">
-    I am mostly working on random projects, you can mostly find them at
-    <a href="https://github.com/me-at-the-labs"><mark>Chle Labs</mark></a>.
-    If you want to contact me, my contact list is below.
-  </span>
-</p>
+              <p id="windowText">
+                <span class="p">
+                  Hi, I'm Guevara (also known as Chle) - an insane developer from Spain.
+                  Over a few years I have been developing some random stuff on the internet.
+                </span>
+                <span class="p">
+                  I am fluent in PHP and JavaScript/TypeScript. I have experience with
+                  React, Vue, and SQL. I can do a bit of Go and Lua too, sometimes Rust,
+                  although they're not on my list.
+                </span>
+              </p>
+
+              <p id="windowText">
+                <span class="p">
+                  I mostly work on random projects; you can find them at
+                  <a href="https://github.com/me-at-the-labs">
+                    <mark>Guevara's Lab</mark>
+                  </a>.
+                  If you want to contact me, my contact list is below.
+                </span>
+              </p>
+
             </div>
           </div>
         </div>
@@ -111,41 +115,5 @@ onUnmounted(() => {
 </script>
 
 <style scoped>
+/* component-specific styles here if needed */
 </style>
-  window.addEventListener('mouseup', stopDrag)
-  window.addEventListener('touchmove', onDrag, { passive: false })
-  window.addEventListener('touchend', stopDrag)
-})
-
-onUnmounted(() => {
-  window.removeEventListener('mousemove', onDrag)
-  window.removeEventListener('mouseup', stopDrag)
-  window.removeEventListener('touchmove', onDrag)
-  window.removeEventListener('touchend', stopDrag)
-})
-</script>
-
-<style scoped>
-/* Component styles handled by global CSS */
-</style>
-
-onMounted(() => {
-  window.addEventListener('mousemove', onDrag)
-  window.addEventListener('mouseup', stopDrag)
-  window.addEventListener('touchmove', onDrag, { passive: false })
-  window.addEventListener('touchend', stopDrag)
-})
-
-onUnmounted(() => {
-  window.removeEventListener('mousemove', onDrag)
-  window.removeEventListener('mouseup', stopDrag)
-  window.removeEventListener('touchmove', onDrag)
-  window.removeEventListener('touchend', stopDrag)
-})
-</script>
-  document.removeEventListener('mousemove', onDrag)
-  document.removeEventListener('mouseup', stopDrag)
-  document.removeEventListener('touchmove', onDrag)
-  document.removeEventListener('touchend', stopDrag)
-})
-</script>
